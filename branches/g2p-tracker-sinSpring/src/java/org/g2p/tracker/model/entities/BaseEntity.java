359b5db5a981680a97f52921ffa9fade43daeda8
juanmanuelcortez <juanmanuelcortez@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-04-23 18:05:28 +0000

    - BaseModel, DataSource management

--- branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/model/entities/BaseEntity.java
+++ branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/model/entities/BaseEntity.java
@@ -9,19 +9,6 @@ package org.g2p.tracker.model.entities;
  *
  * @author Administrador
  */
-public class BaseEntity {
-    int status;
-    public static final int STATUS_NEW = 0;
-    public static final int STATUS_MODIFIED = 1;
-    public static final int STATUS_DELETED = 2;
-    public static final int STATUS_NOT_MODIFIED = 3;
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
+public abstract class BaseEntity {
+    public abstract Object getPK();
 }
