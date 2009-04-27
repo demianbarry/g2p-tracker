be3d4ed02818b7ef7ffdccda50a17e4626d8153a
juanmanuelcortez <juanmanuelcortez@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-04-27 23:59:25 +0000

    - Mejorada generación del la NavBar

--- branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/controllers/BasePageController.java
+++ branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/controllers/BasePageController.java
@@ -4,7 +4,6 @@
  */
 package org.g2p.tracker.controllers;
 
-//~--- non-JDK imports --------------------------------------------------------
 import org.g2p.tracker.model.entities.AccesoMenuEntity;
 
 import org.zkoss.zk.ui.Session;
@@ -17,11 +16,8 @@ import org.zkoss.zul.Separator;
 import org.zkoss.zul.Toolbarbutton;
 import org.zkoss.zul.Vbox;
 
-//~--- JDK imports ------------------------------------------------------------
-
 import java.util.Hashtable;
 import java.util.Iterator;
-import org.g2p.tracker.model.entities.MenuEntity;
 import org.g2p.tracker.model.models.BaseModel;
 
 public class BasePageController extends BaseController implements EventListener {
@@ -38,57 +34,63 @@ public class BasePageController extends BaseController implements EventListener 
     public void onCreate$baseWin(Event event) {
         session = Sessions.getCurrent();
 
+        // Seteo
         session.setAttribute("User", 1);
 
+        // Arranco en la HomePage
         setNavBarItem("HomePage.zul");
     }
 
+    /**
+     * Este método llena la barra de navegación recuperando la información contenida en la base de datos.
+     *
+     * @param groupName La página actual, a través de la cual se recuperan todos sus hijos
+     */
     public void setNavBarItem(String groupName) {
         Toolbarbutton button;
         Hashtable parameters = new Hashtable();
 
+        // Obtengo el userId de la sesión
         parameters.put("userId", session.getAttribute("User"));
+
         parameters.put("GroupName", groupName);
 
+        // Traigo todos los "hijos" de la página actual.
         Iterator menues = BaseModel.findEntities("AccesoMenuEntity.findByUsuarioId", parameters).iterator();
 
-        if (!menues.hasNext()) {
-            parameters.clear();
-            parameters.put("url", groupName);
-            menues = BaseModel.findEntities("MenuEntity.findUrl", parameters).iterator();
-            MenuEntity menu = (MenuEntity) menues.next();
+        // Si el menu actual tiene "hijos", re-armo el menu
+        if (menues.hasNext()) {
+            // Limpio la barra de navegación
+            navBar.getChildren().clear();
 
-            parameters.clear();
-            parameters.put("GroupName", menu.getGrupo());
-            parameters.put("userId", session.getAttribute("User"));
+            // Agrego la referencia a la HomePage, si no estoy en la HomePage
+            if (!"HomePage.zul".equalsIgnoreCase(groupName)) {
+                button = new Toolbarbutton("Página principal");
+                button.setAttribute("page", "HomePage.zul");
+                navBar.appendChild(button);
+                // Seteo el controlador como responsable de capturar el evento de click
+                button.addEventListener("onClick", this);
+            }
 
-            menues = BaseModel.findEntities("AccesoMenuEntity.findByUsuarioId", parameters).iterator();
-        }
+            // Por cada resultado recuperado, creo la referencia en la NavBar
+            while (menues.hasNext()) {
+                AccesoMenuEntity menu = (AccesoMenuEntity) menues.next();
 
+                navBar.appendChild(new Separator());
 
-        System.out.println("------>" + groupName);
-        navBar.getChildren().clear();
-
-        button = new Toolbarbutton("Home Page");
-        button.setAttribute("page", "HomePage.zul");
-        navBar.appendChild(button);
-        button.addEventListener("onClick", this);
-        while (menues.hasNext()) {
-            AccesoMenuEntity menu = (AccesoMenuEntity) menues.next();
-
-
-            navBar.appendChild(new Separator());
-
-            button = new Toolbarbutton(menu.getMenuId().getNombre());
-            button.setAttribute("page", menu.getMenuId().getUrl());
-            button.addEventListener("onClick", this);
-            navBar.appendChild(button);
-
+                button = new Toolbarbutton(menu.getMenuId().getNombre());
+                // agrego un atributo al botón para obtener luego en el listener
+                // del evento la página a la cual tengo que redirigir el "include"
+                button.setAttribute("page", menu.getMenuId().getUrl());
+                button.addEventListener("onClick", this);
+                navBar.appendChild(button);
+            }
         }
     }
 
     @Override
     public void onEvent(Event arg0) throws Exception {
+        // cambio el "include" recuperando el atributo "page" del botón
         include.setSrc(arg0.getTarget().getAttribute("page").toString());
         setNavBarItem(arg0.getTarget().getAttribute("page").toString());
     }
