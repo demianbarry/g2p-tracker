e349febc84e7d32d365dfc17f13fe419261510ee
ignacioaita@gmail.com <ignacioaita@gmail.com@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-05-07 19:55:03 +0000

    OpenID

---
+++ branches/OpenID3.1/src/java/modelo/IOpenID.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.openid4java.discovery.DiscoveryException;
+
+/**
+ *Especifica la interfaz de todos los objetos de tipo OpenID
+ * 
+ * Los objetos OpenID son son un tipo particular Single Sign-on
+ *
+ * @author Cristian Pacheco
+ */
+public interface IOpenID extends ISSO{
+    /**
+     * Redirige al usuario al sitio de autentificacion del proveedor
+     *
+     * @param userSuppliedString url de donde buscar por el servidor de autentificacion
+     * @param httpReq
+     * @param httpResp
+     * @throws java.io.IOException
+     * @throws org.openid4java.discovery.DiscoveryException
+     */
+    public void solicitarAutentificacion(String userSuppliedString,
+                              HttpServletRequest httpReq,
+                              HttpServletResponse httpResp)
+            throws IOException, DiscoveryException;
+}
