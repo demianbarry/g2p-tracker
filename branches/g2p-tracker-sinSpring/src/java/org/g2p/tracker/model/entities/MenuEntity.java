b21d6db2c517b648f8e7ed14a3c6bcf0041f4d69
laykondash@gmail.com <laykondash@gmail.com@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-06-04 07:10:03 +0000

    Se guardan y eliminan los cambios en la base de datos correctamente. Salvo la desasociación de un menu. Todavia queda el problema de la mala actualización de la vista

--- branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/model/entities/MenuEntity.java
+++ branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/model/entities/MenuEntity.java
@@ -12,6 +12,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -31,6 +33,7 @@ import javax.persistence.Table;
 public class MenuEntity extends BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "menu_id")
     private Integer menuId;
