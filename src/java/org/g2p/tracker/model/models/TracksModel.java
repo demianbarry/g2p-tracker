9ab9320e3a9775f735c6914679fece7601dbb459
juanmanuelcortez <juanmanuelcortez@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-06-09 00:21:53 +0000

    - Aplicación de tags. - Recuperación de tags aplicados.

--- src/java/org/g2p/tracker/model/models/TracksModel.java
+++ src/java/org/g2p/tracker/model/models/TracksModel.java
@@ -4,6 +4,7 @@
  */
 package org.g2p.tracker.model.models;
 
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import org.g2p.tracker.model.entities.TagsEntity;
@@ -28,15 +29,45 @@ public class TracksModel extends BaseModel implements Taggeable {
     @Override
     public void aplicarTags(List<TagsEntity> tagsList) throws Exception {
         TracksEntity track = ((TracksEntity) getSelected());
+
+
         if (track != null) {
-            TagsPerTracksEntity tagsPerTrack = null;
+            Hashtable parameters = new Hashtable();
+            parameters.put("trackId", track.getTrackId());
+            List tagsByTrack = BaseModel.findEntities("TagsEntity.findByTrack", parameters);
+
             Iterator<TagsEntity> tags = tagsList.iterator();
+            TagsEntity tag = null;
+            TagsPerTracksEntity tagPerTrack = null;
+
             while (tags.hasNext()) {
-                tagsPerTrack = new TagsPerTracksEntity(
-                        tags.next().getTagId(),
-                        track.getTrackId());
-                create(tagsPerTrack);
+                tag = tags.next();
+                if (!tagsByTrack.contains(tag)) {
+                    tagPerTrack = new TagsPerTracksEntity(
+                            tag.getTagId(),
+                            track.getTrackId());
+                    BaseModel.createEntity(tagPerTrack, true);
+                }
             }
+
+            tags = tagsByTrack.iterator();
+            while (tags.hasNext()) {
+                tag = tags.next();
+                if (!tagsList.contains(tag)) {
+                    tagPerTrack = new TagsPerTracksEntity(
+                            tag.getTagId(),
+                            track.getTrackId());
+                    BaseModel.deleteEntity(tagPerTrack, true);
+                }
+            }
+
         }
+    }
+
+    @Override
+    public List getStoredTags() {
+        Hashtable parameters = new Hashtable();
+        parameters.put("trackId", ((TracksEntity) getSelected()).getTrackId());
+        return BaseModel.findEntities("TagsEntity.findByTrack", parameters);
     }
 }
