8037619a659cde0004d611c06175ee607cbc848a
juanmanuelcortez <juanmanuelcortez@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-06-30 16:36:54 +0000

    - Modificaciones varias en visualizador de tracks. - Ecualización mediante Tags.

--- branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/components/EcualizarEvent.java
+++ branches/g2p-tracker-sinSpring/src/java/org/g2p/tracker/components/EcualizarEvent.java
@@ -4,6 +4,8 @@
  */
 package org.g2p.tracker.components;
 
+import java.util.List;
+import org.g2p.tracker.model.entities.BaseEntity;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 
@@ -13,12 +15,23 @@ import org.zkoss.zk.ui.event.Event;
  */
 public class EcualizarEvent extends Event {
 
+    private List<BaseEntity> entities;
+
+    public List<BaseEntity> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<BaseEntity> entities) {
+        this.entities = entities;
+    }
+
     public EcualizarEvent(String name, Component target, Object data) {
         super(name, target, data);
     }
 
-    public EcualizarEvent(String name, Component target) {
+    public EcualizarEvent(String name, Component target, List<BaseEntity> entities) {
         super(name, target);
+        setEntities(entities);
     }
 
     public String getQuery() {
