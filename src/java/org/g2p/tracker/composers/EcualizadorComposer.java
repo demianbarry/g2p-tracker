024205e3a6b5e50209064b19c6edefa46b1d824d
juanmanuelcortez <juanmanuelcortez@3cdefc62-1d7e-11de-a958-c9a343b5b891>
2009-06-30 16:37:00 +0000

    - Modificaciones varias en visualizador de tracks. - Ecualización mediante Tags.

--- src/java/org/g2p/tracker/composers/EcualizadorComposer.java
+++ src/java/org/g2p/tracker/composers/EcualizadorComposer.java
@@ -4,8 +4,11 @@
  */
 package org.g2p.tracker.composers;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import org.g2p.tracker.controllers.Constants;
 import org.g2p.tracker.model.entities.TagsEntity;
 import org.g2p.tracker.model.models.BaseModel;
@@ -16,13 +19,18 @@ import org.zkoss.zk.ui.event.InputEvent;
 import org.g2p.tracker.components.TagsList;
 import org.g2p.tracker.components.TagsTreeRenderer;
 import org.g2p.tracker.components.TreeModelA;
-import org.g2p.tracker.model.models.Taggeable;
+import org.g2p.tracker.model.models.Ecualizable;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zul.Checkbox;
-import org.zkoss.zul.Groupbox;
+import org.zkoss.zul.Bandbox;
+import org.zkoss.zul.Bandpopup;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Tree;
+import org.zkoss.zul.Treecell;
+import org.zkoss.zul.Treeitem;
 
 /**
  *
@@ -31,7 +39,7 @@ import org.zkoss.zul.Tree;
 public class EcualizadorComposer extends BaseComposer implements Constants {
 
     Tree tagsTree;
-    Groupbox tagsAdmin;
+    Bandbox tagsAdmin;
     TagsTreeRenderer tagsTreeRenderer;
     Listbox tagsList;
     Textbox searchBox;
@@ -49,7 +57,7 @@ public class EcualizadorComposer extends BaseComposer implements Constants {
         tagsTreeList = new TagsList();
 
 
-        if (self instanceof Groupbox) {
+        if (self instanceof Bandbox) {
             tagsTree.setTreeitemRenderer(tagsTreeRenderer);
             populateTagsTree(null);
         }
@@ -72,8 +80,8 @@ public class EcualizadorComposer extends BaseComposer implements Constants {
         return tags;
     }
 
-    private Taggeable getModel() {
-        return (Taggeable) self.getDesktop().getAttribute("TAGGEABLE_MODEL");
+    private Ecualizable getModel() {
+        return (Ecualizable) self.getDesktop().getAttribute("TAGGEABLE_MODEL");
     }
 
     private void populateTagsTree(String criteria) {
@@ -114,15 +122,56 @@ public class EcualizadorComposer extends BaseComposer implements Constants {
         return tagsList;
     }
 
-    public void onChanging$searchBox(InputEvent event) {
+    public void onChanging$ecualizador(InputEvent event) {
         String value = event.getValue();
         populateTagsTree(value);
         setCriteria(value);
         getDataBinder().loadAttribute(tagsList, "model");
+        if(self instanceof Bandbox) {
+            ((Bandbox)self).open();
+        }
     }
 
     public void onClick$ecualizarButton(Event event) {
-        Events.sendEvent(new Event("onEcualizar", (Component) self.getDesktop().getAttribute("ECUALIZABLE_COMPONENT")));
+        Set selectedTags;
+        Hashtable parameters = null;
+        if (tagsList.isVisible()) {
+            selectedTags = tagsList.getSelectedItems();
+            parameters = new Hashtable();
+        } else {
+            selectedTags = tagsTree.getSelectedItems();
+        }
+        if (getModel() != null) {
+            List<TagsEntity> tagsList = new ArrayList();
+            if (selectedTags.size() > 0) {
+                Object item;
+                TagsEntity tag = null;
+                Iterator tagsIterator = selectedTags.iterator();
+                while (tagsIterator.hasNext()) {
+                    try {
+                        item = tagsIterator.next();
+                        if (item instanceof Treeitem) {
+                            tag = (TagsEntity) ((Treecell) ((Treeitem) item).getTreerow().getChildren().get(0)).getAttribute(TAG_ID);
+                        } else {
+                            parameters.clear();
+                            parameters.put("tag", ((Listcell) ((Listitem) item).getChildren().get(0)).getLabel());
+                            tag = (TagsEntity) (BaseModel.findEntities("TagsEntity.findByTag", parameters).get(0));
+                        }
+                        tagsList.add(tag);
+                    } catch (Exception ex) {
+                        showMessage("ERROR: ", ex);
+                    }
+                }
+            }
+            try {
+                Events.sendEvent(
+                        new Event("onEcualizar",
+                        (Component) self.getDesktop().getAttribute("ECUALIZABLE_COMPONENT"),
+                        getModel().ecualizarByTags(tagsList)));
+            } catch (Exception ex) {
+                showMessage("ERROR aplicando tags: ", ex);
+            }
+        }
     }
 
     public void onCheck$treeCheck(CheckEvent event) {
